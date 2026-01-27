5e1ab9952de03101b159f36db36bdd488c6476a3
Dayen Al Fiansyach <veydayenfatzla@gmail.com>
2026-01-27 22:55:14 +0700

    on progress

--- app/(landing)/components/ui/cardWithHeader.tsx
+++ app/(landing)/components/ui/cardWithHeader.tsx
@@ -8,7 +8,7 @@ type TCardWithHeaderProps = {
 const CardWithHeader = ({ header, children }: TCardWithHeaderProps) => {
   return (
     <div className="pt-4 pb-13 bg-white">
-      <h2 className="pb-3.5 px-5 border-b border-[#E4E4E4D1]/82 mb-5 font-bold text-xl">
+      <h2 className="pb-3.5 px-5 border-b border-[#E4E4E4D1]/82  font-bold text-xl">
         {header}
       </h2>
       {children}
